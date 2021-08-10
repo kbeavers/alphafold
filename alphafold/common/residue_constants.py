f3981311dcd2285ede94ff505cac419b51ad8ec2
Erik Ferlanti <eferlanti@tacc.utexas.edu>
2021-08-10 12:40:22 -0500

    fixed path to alphafold/common/stereo_chemical_props.txt switched to tensorflow gpu version

--- alphafold/common/residue_constants.py
+++ alphafold/common/residue_constants.py
@@ -403,7 +403,7 @@ def load_stereo_chemical_props() -> Tuple[Mapping[str, List[Bond]],
     residue_bond_angles: dict that maps resname --> list of BondAngle tuples
   """
   stereo_chemical_props_path = (
-      'alphafold/common/stereo_chemical_props.txt')
+      '/app/alphafold/alphafold/common/stereo_chemical_props.txt')
   with open(stereo_chemical_props_path, 'rt') as f:
     stereo_chemical_props = f.read()
   lines_iter = iter(stereo_chemical_props.splitlines())
